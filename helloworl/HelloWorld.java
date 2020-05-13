d923c5899da7aca525ed88e645ed98766346c149
daniel <daniel.lin.gr@gmail.com>
2020-05-13 14:00:14 +0800

    Update HelloWorld.java

--- helloworl/HelloWorld.java
+++ helloworl/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
     
 
 # javac HelloWorld.java
-# 編譯完成後，同一個目錄下會產生一個HelloWorld.class檔 案，在執行時期時可以由執行環境轉換為平台可執行的格式，    
-# 注意最後並沒有加上*.class的副檔名，您只要提供主檔名就可以了，程式畫面會顯示如下：
+# 編譯完成後，會產生一個 HelloWorld.class檔案
+# java HelloWorld
+# 會輸出下列訊息
 # Hello! World!
