12486f7d7f2b8be48d02f599695b629d4c9bce68
daniel <daniel.lin.gr@gmail.com>
2020-05-13 14:17:23 +0800

    Create HelloWorld.java

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello! World!");
+    }
+    
+    
+
+# javac HelloWorld.java
+# 編譯完成後，會產生一個 HelloWorld.class檔案
+# java HelloWorld
+# 會輸出下列訊息
+# Hello! World!
