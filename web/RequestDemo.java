41185acd3f141069c60040a3ee9fc79e5b3e6246
daniel <daniel.lin.gr@gmail.com>
2020-05-27 11:11:14 +0800

    Update RequestDemo.java

--- web/RequestDemo.java
+++ web/RequestDemo.java
@@ -1,3 +1,8 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 public class RequestDemo{
 public static void main(String[] args){
         System.out.println("Hello World!");
