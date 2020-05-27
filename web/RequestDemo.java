cdd72714bcaf2b8a9cf3cecb67bc35340d0bf94d
daniel <daniel.lin.gr@gmail.com>
2020-05-27 11:45:27 +0800

    Update RequestDemo.java

--- web/RequestDemo.java
+++ web/RequestDemo.java
@@ -6,5 +6,14 @@ import org.apache.http.impl.client.HttpClients;
 public class RequestDemo{
 public static void main(String[] args){
         System.out.println("Hello World!");
+        
+        //Creating a HttpClient object
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //Creating a HttpGet object
+        HttpPost httppost = new HttpPost("http://www.kaops.com/");
+
+        //Printing the method used
+        System.out.println("Request Type: "+httppost.getMethod());
 }
 }
