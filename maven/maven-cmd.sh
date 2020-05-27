6965c679cbe02dc3edb5b61e72bc2f74515d25db
daniel <daniel.lin.gr@gmail.com>
2020-05-27 10:17:19 +0800

    Update maven-cmd.sh

--- maven/maven-cmd.sh
+++ maven/maven-cmd.sh
@@ -5,8 +5,13 @@
 # 打包 , jar 檔會在 target 目錄下 
 mvn package
 
-# 執行打包後的 java
+# 執行打包後的 java         
+# com.habook.App , 是 package Name，加上 Class Name
+
 java -cp target/NumberGenerator-1.0-SNAPSHOT.jar com.yiibai.App
 
 java -cp  firstMaven-1.0-SNAPSHOT.jar  com.habook.App
+
+
+
 
